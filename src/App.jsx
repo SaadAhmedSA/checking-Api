d6359c3dd42c438f168558d14b9ba50283e731ba
Saad Ahmed <ss5099903@gmail.com>
2024-11-26 21:33:20 -0800

    data store on mongodb

--- src/App.jsx
+++ src/App.jsx
@@ -7,53 +7,96 @@ const App = () => {
   const [error , seterror] = useState(false)
   useEffect(()=>{
     
-      axios.get("http://localhost:3000/users")
-      .then((res) =>setdata(res.data.data),
+      axios.get("http://localhost:3000/api/users")
+      .then((res) =>setdata(res.data),
         
       
-      ).catch(err => seterror(true)
-       
-
-      ).finally(
-         setloading(false)
-      )
+    ).catch(err => seterror(true)
     
+    
+  ).finally(
+    setloading(false)
+  )
+  
+  data && console.log(data)
 
   },[])
-  console.log(data);
   
-  const todoval = useRef()
+  const username = useRef()
+  const email = useRef()
+  const password = useRef()
   const Addtodo = (e)=>{
     e.preventDefault()
-    axios.post("http://localhost:3000/user",{
-      title : todoval.current.value
+   try {
+    axios.post("http://localhost:3000/api/user",{
+      username : username.current.value,
+      email : email.current.value,
+      password : password.current.value,
+      
     })
-    todoval.current.value=""
+    console.log("data added");
+    
+   } catch (error) {
+    console.log(err);
+    
+   }
+  
   }
   const  Deltodo = (id)=>{
-    axios.delete(`http://localhost:3000/user/${id}`)
+    try {
+      axios.delete(`http://localhost:3000/api/user/${id}`)
+      console.log("deleted");
+      
+    } catch (error) {
+
+      console.log(error);
+      
+    }
+  }
+  const  detail = async (id)=>{
+    try {
+     const data = await axios.get(`http://localhost:3000/api/user/${id}`)
+      console.log(data.data);
+      
+    } catch (error) {
+
+      console.log(error);
+      
+    }
   }
   const  Edittodo = (id,value)=>{
-     const upt = prompt("Enter Value")
-    axios.put(`http://localhost:3000/user/${id}`,{
-      title : upt
-    })
+     const username = prompt("Enter Value")
+   try {
+    axios.put(`http://localhost:3000/api/user/${id}`,{
+      username,
+     })
+     console.log("update");
+     
+   } catch (error) {
+    console.log(error);
+    
+   }
   }
   return (
     <div>
       <h1>Todo App</h1>
       <form onSubmit={Addtodo} >
-        <input type="text" placeholder='Enter Data' ref={todoval}/>
+        <input type="text" placeholder='Enter username' ref={username}/>
+        <input type="email" placeholder='Enter email' ref={email}/>
+        <input type="password" placeholder='Enter password' ref={password}/>
         <button type='submit'>Add todo</button>
       </form>
       {loading && <h3>Loading</h3>}
       {error && <h3>Error Occured</h3>}
       {data && data.length > 0 ? data.map((item)=>{
         return(
-          <div key={item.id}>
-          <p>{item.title}</p>
-          <button onClick={()=>Deltodo(item.id)}>Delete</button>
-          <button onClick={()=>Edittodo(item.id,item.title)}>Edit</button>
+          <div key={item._id} style={{border: "1px solid black",padding:20, margin :20 ,borderRadius:20}}>
+          <p>{item.username}</p>
+          <p>{item.email}</p>
+          <p>{item.password}</p>
+          <button onClick={()=>detail(item._id)}>see detail</button>
+          <button onClick={()=>Deltodo(item._id)}>Delete</button>
+          <button onClick={()=>Edittodo(item._id,)}>Edit</button>
         </div>
         )
 
